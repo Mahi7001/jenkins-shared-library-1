bf9bb9086c2602cf364837f9404ec8ebf6751f21
mahesh patil <87996248+Mahi7001@users.noreply.github.com>
2023-01-11 15:38:22 +0530

    Update BuildImage.groovy

--- vars/BuildImage.groovy
+++ vars/BuildImage.groovy
@@ -2,7 +2,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials() {
         sh 'docker build -t mahesh2024/demo-app:jma-2.0 .'
-        sh "echo $dockerpasswd1 | docker login -u mahesh2024 --password-stdin"
+        sh 'docker login -u mahesh2024 -p ${dockerpasswd1}
         sh 'docker push mahesh2024/demo-app:jma-2.0'
     }
 } 
